077441e2cc2dacfbf7b33b969e65079a3212edf9
Shimul <shimul39ju@gmail.com>
2017-02-06 12:17:51 +0600

    Connection String Add

--- BillsPayableSystem/DbGateway/ConnectionString.cs
+++ BillsPayableSystem/DbGateway/ConnectionString.cs
@@ -9,8 +9,9 @@ namespace BillsPayableSystem.DbGateway
    public class ConnectionString
     {
        //For Live
-       public string DBConn = @"Data Source=tcp:kyotoServer,49172;Initial Catalog=BillsPayableDB;User ID=sa;Password=SystemAdministrator;Persist Security Info=True";
-
+       //public string DBConn = @"Data Source=tcp:kyotoServer,49172;Initial Catalog=BillsPayableDB;User ID=sa;Password=SystemAdministrator;Persist Security Info=True";
+       //Dummy
+     public string DBConn = @"Data Source=tcp:kyotoServer,49172;Initial Catalog=BillsPayableDB_new;User ID=sa;Password=SystemAdministrator;Persist Security Info=True";
        //For Local
        //public string DBConn = @"Data Source=KYOTO-PC99;Initial Catalog=BillsPayableDB;Persist Security Info=True;User ID=sa;Password=SystemAdministrator";
        
