23cf146c2a7d22f935f2e2f3385fbe351f65f058
Runju Ahmed <Runju@kyoto.local>
2017-04-26 11:54:40 +0600

    UnPaidBillReport

---
+++ BillsPayableSystem/UI/ReportUI.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace BillsPayableSystem.UI
+{
+    partial class ReportUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.unpaidBillListButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // unpaidBillListButton
+            // 
+            this.unpaidBillListButton.BackColor = System.Drawing.Color.LightCyan;
+            this.unpaidBillListButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.unpaidBillListButton.Location = new System.Drawing.Point(111, 60);
+            this.unpaidBillListButton.Name = "unpaidBillListButton";
+            this.unpaidBillListButton.Size = new System.Drawing.Size(186, 63);
+            this.unpaidBillListButton.TabIndex = 0;
+            this.unpaidBillListButton.Text = "Unpaid Bill List";
+            this.unpaidBillListButton.UseVisualStyleBackColor = false;
+            this.unpaidBillListButton.Click += new System.EventHandler(this.unpaidBillListButton_Click);
+            // 
+            // ReportUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(791, 396);
+            this.Controls.Add(this.unpaidBillListButton);
+            this.Name = "ReportUI";
+            this.Text = "ReportUI";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button unpaidBillListButton;
+    }
+}
---
+++ BillsPayableSystem/UI/ReportUI.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BillsPayableSystem.Reports;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using Microsoft.Reporting.WinForms;
+using ReportViewer = BillsPayableSystem.Reports.ReportViewer;
+
+namespace BillsPayableSystem.UI
+{
+    public partial class ReportUI : Form
+    {
+        public ReportUI()
+        {
+            InitializeComponent();
+        }
+
+        private void unpaidBillListButton_Click(object sender, EventArgs e)
+        {
+            //creating an object of ParameterField class
+            ParameterField paramField = new ParameterField();
+
+            //creating an object of ParameterFields class
+            ParameterFields paramFields = new ParameterFields();
+
+            //creating an object of ParameterDiscreteValue class
+            ParameterDiscreteValue paramDiscreteValue = new ParameterDiscreteValue();
+
+            //set the parameter field name
+            //paramField.Name = "Meeting No.";
+
+            //set the parameter value
+            //paramDiscreteValue.Value = x;
+
+            //add the parameter value in the ParameterField object
+            paramField.CurrentValues.Add(paramDiscreteValue);
+
+            //add the parameter in the ParameterFields object
+            paramFields.Add(paramField);
+
+            //set the parameterfield information in the crystal report
+
+
+
+            ReportViewer f2 = new ReportViewer();
+            TableLogOnInfos reportLogonInfos = new TableLogOnInfos();
+            TableLogOnInfo reportLogonInfo = new TableLogOnInfo();
+            ConnectionInfo reportConInfo = new ConnectionInfo();
+            Tables tables = default(Tables);
+            //	Table table = default(Table);
+            var with1 = reportConInfo;
+            with1.ServerName = "tcp:KyotoServer,49172";
+            with1.DatabaseName = "BillsPayableDB";
+            with1.UserID = "sa";
+            with1.Password = "SystemAdministrator";
+            UnpaidBillListReport cr = new UnpaidBillListReport();
+            tables = cr.Database.Tables;
+            foreach (Table table in tables)
+            {
+                reportLogonInfo = table.LogOnInfo;
+                reportLogonInfo.ConnectionInfo = reportConInfo;
+                table.ApplyLogOnInfo(reportLogonInfo);
+            }
+            f2.crystalReportViewer1.ParameterFieldInfo = paramFields;
+            //set the parameterfield information in the crystal report
+            f2.crystalReportViewer1.ReportSource = cr;
+            this.Visible = false;
+
+            f2.ShowDialog();
+            this.Visible = true;
+
+        }
+    }
+}
